1ad42a6b7638d23ef561d162ff7675b691ee1fee
Thomas Perl <m@thp.io>
2013-04-22 15:10:12 +0200

    Properly bracket macro parameters in TOLOWER

--- compat/libc_wrappers.c
+++ compat/libc_wrappers.c
@@ -497,7 +497,7 @@ my_srand48(long int __seedval)
 int
 my_strcasecmp(__const char *__s1, __const char *__s2)
 {
-#define TOLOWER(x) ((x >= 'A') && (x <= 'Z')) ? (x - 'A' + 'a') : x
+#define TOLOWER(x) ((((x) >= 'A') && ((x) <= 'Z')) ? ((x) - 'A' + 'a') : (x))
     while(('\0' != *__s1) && ('\0' != *__s2))
     {
         char c1 = TOLOWER(*__s1), c2 = TOLOWER(*__s2);
